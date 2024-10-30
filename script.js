ff706c7df955eb26519fd7049606d409d4e30943
Razvan Ciocan <rzv.c93@outlook.com>
2024-10-30 17:48:37 +0000

    Fix the variable name

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function changeBtnColor() {
     function updateBtnColor() {
         ++clickCount;
         if (clickCount === 1) {
-            btn.classList.remove("btn-danger", "btn-success");
-            btn.classList.add("btn-warning"); 
+            selectedBtn.classList.remove("btn-danger", "btn-success");
+            selectedBtn.classList.add("btn-warning"); 
         } else if (clickCount === 2) {
-            btn.classList.remove("btn-warning");
-            btn.classList.add("btn-success"); 
+            selectedBtn.classList.remove("btn-warning");
+            selectedBtn.classList.add("btn-success"); 
         } else {
-            btn.classList.remove("btn-success");
-            btn.classList.add("btn-danger");
+            selectedBtnbtn.classList.remove("btn-success");
+            selectedBtnbtn.classList.add("btn-danger");
             clickCount = 0; 
         }
     }
-    btn.addEventListener("click", updateBtnColor);
+    selectedBtn.addEventListener("click", updateBtnColor);
     setInterval(updateBtnColor, 10000);
 }
 changeBtnColor();
