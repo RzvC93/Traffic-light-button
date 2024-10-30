e6c393c80fc77b4e84df831a2bc233ebd93fe26f
Razvan Ciocan <rzv.c93@outlook.com>
2024-10-30 16:48:25 +0000

    Remove repetitive code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+function changeBtnColor() {
+    const selectedBtn = document.getElementById("btn");
+    let clickCount = 0;
 
-function changeButonColor() {
-    const btn = document.getElementById("btn");
-    let clickCount = 0; 
-
-    btn.addEventListener("click", function() {
+    function updateBtnColor() {
         ++clickCount;
         if (clickCount === 1) {
             btn.classList.remove("btn-danger", "btn-success");
@@ -16,21 +15,8 @@ function changeButonColor() {
             btn.classList.add("btn-danger");
             clickCount = 0; 
         }
-    });
-
-    setInterval(function() {
-        clickCount++; 
-        if (clickCount === 1) {
-            btn.classList.remove("btn-danger", "btn-success");
-            btn.classList.add("btn-warning"); 
-        } else if (clickCount === 2) {
-            btn.classList.remove("btn-warning");
-            btn.classList.add("btn-success"); 
-        } else {
-            btn.classList.remove("btn-success");
-            btn.classList.add("btn-danger"); 
-            clickCount = 0;
-        }
-    }, 10000);
+    }
+    btn.addEventListener("click", updateBtnColor);
+    setInterval(updateBtnColor, 10000);
 }
-changeButonColor();
+changeBtnColor();
