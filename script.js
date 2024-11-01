5fefca786736bb89e0cb81e5fa77d5e7d853913b
Razvan Ciocan <rzv.c93@outlook.com>
2024-11-01 17:12:46 +0000

    Final version, get rid of the else-if by saving the colors in an array, using console.log() to check if the element is selected correctly

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 function changeBtnColor() {
     const selectedBtn = document.getElementById("btn");
+    const colours = ["btn-warning", "btn-success", "btn-danger"];
     let clickCount = 0;
 
     function updateBtnColor() {
-        ++clickCount;
-        if (clickCount === 1) {
-            selectedBtn.classList.remove("btn-danger", "btn-success");
-            selectedBtn.classList.add("btn-warning"); 
-        } else if (clickCount === 2) {
-            selectedBtn.classList.remove("btn-warning");
-            selectedBtn.classList.add("btn-success"); 
-        } else {
-            selectedBtnbtn.classList.remove("btn-success");
-            selectedBtnbtn.classList.add("btn-danger");
-            clickCount = 0; 
-        }
+        selectedBtn.classList.remove(...colours);
+        selectedBtn.classList.add(colours[clickCount]);
+        clickCount = (clickCount + 1) % colours.length;
+        console.log("test");
     }
     selectedBtn.addEventListener("click", updateBtnColor);
     setInterval(updateBtnColor, 10000);
+    console.log("test interval");
 }
 changeBtnColor();
